fa920ac4cf8238fb71c677c2e6a7dc6b7cc6af9c
NicEastvillage <nicoesterby@gmail.com>
2019-05-19 14:44:30 +0200

    Added rotation and prediction.

--- sniper/sniper.py
+++ sniper/sniper.py
@@ -7,7 +7,7 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 from RLUtilities.LinearAlgebra import *
 
-SPEED = 2000
+SPEED = 2500
 AIM_DURATION = 1.5
 
 
@@ -26,30 +26,45 @@ class SniperBot(BaseAgent):
         self.shoot_time = 0
         self.last_pos = self.standby_position
         self.last_elapsed_seconds = 0
+        self.last_gtr = 0
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         dt = packet.game_info.seconds_elapsed - self.last_elapsed_seconds
+        dt_gtr = packet.game_info.game_time_remaining - self.last_gtr
+        self.last_gtr = packet.game_info.game_time_remaining
         self.last_elapsed_seconds = packet.game_info.seconds_elapsed
         self.info.read_packet(packet)
 
         ball_pos = self.info.ball.pos
 
+        print(packet.game_info.is_kickoff_pause, packet.game_info.is_round_active, dt == 0, packet.game_info.game_time_remaining, dt_gtr, packet.game_info.seconds_elapsed)
+
         if ball_pos[0] == 0 and ball_pos[1] == 0 and packet.game_info.is_kickoff_pause:
-            # Kickoff
+            # Kickoff and pauses
+            self.state = self.AIMING
             self.shoot_time = self.info.time + AIM_DURATION
             self.controls.boost = False
 
         elif self.state == self.AIMING:
 
             self.controls.boost = False
+            ball_pos = self.predicted_ball_pos()
+            self.direction = d = normalize(ball_pos - self.standby_position)
 
-            car_state = CarState(Physics(location=to_fb(self.standby_position), velocity=Vector3(0, 0, 0)))
+            rotation = Rotator(0, math.atan2(d[1], d[0]), 0)
+            car_state = CarState(Physics(location=to_fb(self.standby_position),
+                                         velocity=Vector3(0, 0, 0),
+                                         rotation=rotation,
+                                         angular_velocity=Vector3(0, 0, 0)))
             game_state = GameState(cars={self.index: car_state})
             self.set_game_state(game_state)
 
+            self.renderer.begin_rendering()
+            self.renderer.draw_rect_3d(ball_pos, 10, 10, True, self.renderer.team_color(), True)
+            self.renderer.end_rendering()
+
             if self.shoot_time < self.info.time:
                 self.state = self.FLYING
-                self.direction = normalize(ball_pos - self.standby_position)
 
         elif self.state == self.FLYING:
 
@@ -68,6 +83,21 @@ class SniperBot(BaseAgent):
                 self.last_pos = self.standby_position
 
         return self.controls
+
+    def predicted_ball_pos(self):
+        ball_prediction = self.get_ball_prediction_struct()
+
+        if ball_prediction is not None:
+            SLICES_PER_SEC = 60
+            SECONDS = 6
+
+            dist = norm(self.info.my_car.pos - self.info.ball.pos)
+            time = dist / SPEED
+            slice_index = min(max(0, math.floor(SLICES_PER_SEC * time)), SLICES_PER_SEC * SECONDS - 1)
+            pos = ball_prediction.slices[slice_index].physics.location
+            return vec3(pos.x, pos.y, pos.z)
+
+        return vec3(0, 0, 0)
 
 
 def to_fb(vec: vec3):
